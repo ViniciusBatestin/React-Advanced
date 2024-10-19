af5c0ba458687e582dde6d6d4260b9e27efe6d68
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-10-19 11:46:01 +0200

    rendering lists in react

--- basic-list/src/DessertsList.js
+++ basic-list/src/DessertsList.js
@@ -1,6 +1,15 @@
 function DessertsList(props) {
   // Implement the component here.
-  return null;
-}
+  var lowCalories = props.data.filter(dessert => {
+    return dessert.calories < 500
+  })
+  .sort((a, b) => {
+    return a.calories - b.calories;
+  })
+  .map((dessert) => {
+    return <li>{dessert.name} - {dessert.calories}</li>;
+  })
 
+  return<ul>{lowCalories}</ul>
+}
 export default DessertsList;
